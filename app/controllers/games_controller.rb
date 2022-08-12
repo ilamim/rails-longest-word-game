7d31931927e9e06393465b89c9a9d91fa3727c08
Isabel Lamim <lamim@protonmail.com>
2022-08-12 16:22:00 +0200

    seemingly functional version of longest word

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,27 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
     @grid = Array.new(10) { ('A'..'Z').to_a.sample }
   end
 
+  def valid_attempt?(attempt)
+    check_dictionary = URI.open("https://wagon-dictionary.herokuapp.com/#{attempt.downcase}")
+    valid_word = JSON.parse(check_dictionary.read)
+    return valid_word["found"]
+  end
+
   def score
-    @message = "TEST"
+    @message = "NOPE"
+    @attempt_grid = params[:attempt_grid]
+    @attempt_word = params[:user_word].upcase
+    if !(@attempt_word.chars.all? { |letter| @attempt_word.count(letter) <= @attempt_grid.count(letter) })
+      @message = "Your attempt does not seem to be contained in the grid: #{@attempt_grid}"
+    elsif !valid_attempt?(@attempt_word)
+      @message = "Your attempt does not seem to be an English word."
+    else
+      @message = "Congratulations! #{@attempt_word} is a valid attempt."
+    end
   end
 end
